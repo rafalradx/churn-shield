831336d75c2fe0e0801ac42af36cb7920abd9cd7
Rafal Pietras <rafal.radx@gmail.com>
2024-08-24 14:52:33 +0200

    fastatpi static files quickfix

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-from src.routes import models, gpt,views
+from fastapi.staticfiles import StaticFiles
+from src.routes import models, gpt, views
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 app.include_router(gpt.router)
 app.include_router(models.router)
 app.include_router(views.router)
--- src/routes/views.py
+++ src/routes/views.py
@@ -1,13 +1,11 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from src.repository.models import models
 
 router = APIRouter(tags=["views"])
 
 templates = Jinja2Templates(directory="templates")
-router.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @router.get("/", response_class=HTMLResponse)
